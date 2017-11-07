8371fe5f5a991299c8cf25dcc85e9dbedd7a71d1
tcouch <t.couch@ucl.ac.uk>
2017-11-07 23:45:57 +0000

    basic layout
    Basic layout complete and weekday labels autogenerated from list.

--- d3_code_cal.js
+++ d3_code_cal.js
@@ -3,7 +3,7 @@ var   w = 800,
       square = 60;
 var   weekdayLabelHeight = square/3;
 
-var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
 var margin = {left:10,right:10,top:10,bottom:0};
 var internalCalendarMargin = 10;
@@ -27,46 +27,54 @@ console.log(firstDayName);
 
 
 // create the calendar
-var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
-
-var calendar = svg.append("g").attr("id","calendar")
-                    .attr("transform","translate("+margin.left+","+margin.top+")");
-
-
-calendar.selectAll(".weekdayLabel")
+d3.select("#weekdayLabels").selectAll(".dayOfWeek")
   .data(weekday)
   .enter()
-    .append("g")
-      .attr("class","weekdayLabel")
-      .append("rect")
-      .attr("height",weekdayLabelHeight)
-      .attr("width",square)
-      .attr("fill","blue")
-      .attr("x",function(d,i){ return (square+internalCalendarMargin)*((i+6)%7); })
-      .attr("y",0);
+    .append("div")
+      .attr("class","dayOfWeek")
+      .attr("id",function(d){return d;})
+      .html(function(d){ return d ;});
 
-calendar.selectAll(".weekdayLabel")
-  .append("text")
-  .attr("transform",function(d,i){ return "translate("+((square+internalCalendarMargin)*((i+6)%7)+5)+",12)";})
-  .attr("font-size","0.5em")
-  .attr("fill", "white")
-  .text(function(d,i){ return weekday[i];});
-
-calendar.selectAll(".day")
-  .data(days)
-  .enter()
-    .append("g")
-      .attr("class","day")
-      .append("rect")
-      .attr("height",square)
-      .attr("width",square)
-      .attr("fill","pink")
-      .attr("x",function(d,i){ return (square+internalCalendarMargin)*((firstDayNumber+i-1)%7); })
-      .attr("y",function(d,i){ return (Math.floor((i+firstDayNumber-1)/7)*(square+internalCalendarMargin))+weekdayLabelHeight+internalCalendarMargin});
-
-calendar.selectAll(".day")
-  .append("text")
-  .attr("transform",function(d,i){ return "translate("+((square+internalCalendarMargin)*((firstDayNumber+i-1)%7)+2)+","+((Math.floor((firstDayNumber+i-1)/7)*(square+internalCalendarMargin)+15)+weekdayLabelHeight+internalCalendarMargin)+")";})
-  .attr("font-size","1em")
-  .attr("color", "black")
-  .text(function(d,i){ return (i+1).toString();});
+// var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+//
+// var calendar = svg.append("g").attr("id","calendar")
+//                     .attr("transform","translate("+margin.left+","+margin.top+")");
+//
+//
+// calendar.selectAll(".weekdayLabel")
+//   .data(weekday)
+//   .enter()
+//     .append("g")
+//       .attr("class","weekdayLabel")
+//       .append("rect")
+//       .attr("height",weekdayLabelHeight)
+//       .attr("width",square)
+//       .attr("fill","blue")
+//       .attr("x",function(d,i){ return (square+internalCalendarMargin)*((i+6)%7); })
+//       .attr("y",0);
+//
+// calendar.selectAll(".weekdayLabel")
+//   .append("text")
+//   .attr("transform",function(d,i){ return "translate("+((square+internalCalendarMargin)*((i+6)%7)+5)+",12)";})
+//   .attr("font-size","0.5em")
+//   .attr("fill", "white")
+//   .text(function(d,i){ return weekday[i];});
+//
+// calendar.selectAll(".day")
+//   .data(days)
+//   .enter()
+//     .append("g")
+//       .attr("class","day")
+//       .append("rect")
+//       .attr("height",square)
+//       .attr("width",square)
+//       .attr("fill","pink")
+//       .attr("x",function(d,i){ return (square+internalCalendarMargin)*((firstDayNumber+i-1)%7); })
+//       .attr("y",function(d,i){ return (Math.floor((i+firstDayNumber-1)/7)*(square+internalCalendarMargin))+weekdayLabelHeight+internalCalendarMargin});
+//
+// calendar.selectAll(".day")
+//   .append("text")
+//   .attr("transform",function(d,i){ return "translate("+((square+internalCalendarMargin)*((firstDayNumber+i-1)%7)+2)+","+((Math.floor((firstDayNumber+i-1)/7)*(square+internalCalendarMargin)+15)+weekdayLabelHeight+internalCalendarMargin)+")";})
+//   .attr("font-size","1em")
+//   .attr("color", "black")
+//   .text(function(d,i){ return (i+1).toString();});
