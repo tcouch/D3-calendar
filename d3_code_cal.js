feed9ca7bd75f5b4b7ba6351bbf5f1809810f93e
tcouch <t.couch@ucl.ac.uk>
2017-11-29 08:52:18 -0800

    slightly working calendar api integration

--- d3_code_cal.js
+++ d3_code_cal.js
@@ -13,6 +13,7 @@ var today = new Date();
 var date = today.getDate(),
     month = today.getMonth(),
     year = today.getFullYear();
+var tomorrow = new Date(year,month,date+1);
 
 // Get Client ID and API key from secrets.json
 var secrets = JSON.parse(secrets);
@@ -25,6 +26,9 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
 
 
 function daysInMonth(iMonth, iYear)
@@ -113,7 +117,7 @@ function selectBox() {
   selected.classed("selected",true);
   selected.data = selected.datum();
   d3.select("#detailBox")
-    .html(function(){ return "<h3>"+selected.data["longName"]+"</h3>";});
+    .html(function(){ return "<h3>"+selected.data["longName"]+"</h3><pre id='content'></pre>";});
 }
 
 // create the calendar
@@ -167,18 +171,109 @@ var todayBoxNumber = getBoxNumber(date,month,year);
 var selected = d3.select("#box"+todayBoxNumber)
 selected.each(selectBox);
 
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+*  Initializes the API client library and sets up sign-in state
+*  listeners.
+*/
 function initClient() {
   gapi.client.init({
     apiKey: API_KEY,
     clientId: CLIENT_ID,
     discoveryDocs: DISCOVERY_DOCS,
     scope: SCOPES
-  })
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  });
 }
 
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listUpcomingEvents();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
 }
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+*  Sign out the user upon button click.
+*/
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+* Append a pre element to the body containing the given message
+* as its text node. Used to display the results of the API call.
+*
+* @param {string} message Text to be placed in pre element.
+*/
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+* Print the summary and start datetime/date of the next ten events in
+* the authorized user's calendar. If no events are found an
+* appropriate message is printed.
+*/
+function listUpcomingEvents() {
+  gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': today.toISOString(),
+    'timeMax': tomorrow.toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  }).then(function(response) {
+    var events = response.result.items;
+    appendPre('Upcoming events:');
+
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
+  });
+}
+
+
 
 console.log(typeof(Storage));
 localStorage.setItem('thing1','[1,2,3,4,5]');
